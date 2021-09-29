5a0c2d0fd0caaee6236610f3c74a6e729d1d62e2
Josep Servat <jservat@outlook.es>
2021-09-29 11:26:28 +0200

    good contract

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -1,22 +1,21 @@
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity 0.6.3;
 pragma experimental ABIEncoderV2;
 
-import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol';
-import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/math/SafeMath.sol';
 
 contract Dex {
 
     using SafeMath for uint;
 
+    enum Side {
+        BUY,
+        SELL
+    }
 
     struct Token {
         bytes32 ticker;
         address tokenAddress;
-    }
-
-    enum Side {
-        BUY,
-        SELL
     }
 
     struct Order {
@@ -54,12 +53,43 @@ contract Dex {
         admin = msg.sender;
     }
 
-    function addToken(bytes32 ticker, address tokenAddress) onlyAdmin() external {
+    function getOrders(
+      bytes32 ticker,
+      Side side)
+      external
+      view
+      returns(Order[] memory) {
+      return orderBook[ticker][uint(side)];
+    }
+
+    function getTokens()
+      external
+      view
+      returns(Token[] memory) {
+      Token[] memory _tokens = new Token[](tokenList.length);
+      for (uint i = 0; i < tokenList.length; i++) {
+        _tokens[i] = Token(
+          tokens[tokenList[i]].ticker,
+          tokens[tokenList[i]].tokenAddress
+        );
+      }
+      return _tokens;
+    }
+
+    function addToken(
+        bytes32 ticker,
+        address tokenAddress)
+        onlyAdmin()
+        external {
         tokens[ticker] = Token(ticker, tokenAddress);
         tokenList.push(ticker);
     }
 
-    function deposit(uint amount, bytes32 ticker) tokenExist(ticker) external {
+    function deposit(
+        uint amount,
+        bytes32 ticker)
+        tokenExist(ticker)
+        external {
         IERC20(tokens[ticker].tokenAddress).transferFrom(
             msg.sender,
             address(this),
@@ -68,29 +98,36 @@ contract Dex {
         traderBalances[msg.sender][ticker] = traderBalances[msg.sender][ticker].add(amount);
     }
 
-    function withdraw(uint amount, bytes32 ticker) tokenExist(ticker)  external {
+    function withdraw(
+        uint amount,
+        bytes32 ticker)
+        tokenExist(ticker)
+        external {
         require(
             traderBalances[msg.sender][ticker] >= amount,
-            'balance token too low'
+            'balance too low'
         );
         traderBalances[msg.sender][ticker] = traderBalances[msg.sender][ticker].sub(amount);
-        IERC20(tokens[ticker].tokenAddress).transfer(
-            msg.sender,
-            amount
-        );
+        IERC20(tokens[ticker].tokenAddress).transfer(msg.sender, amount);
     }
 
-    function createLimitOrder(bytes32 ticker, uint amount, uint price, Side side) tokenExist(ticker) tokenIsNotDai(ticker) external {
-        require(ticker != DAI, 'cannot trade DAI');
+    function createLimitOrder(
+        bytes32 ticker,
+        uint amount,
+        uint price,
+        Side side)
+        tokenExist(ticker)
+        tokenIsNotDai(ticker)
+        external {
         if(side == Side.SELL) {
             require(
                 traderBalances[msg.sender][ticker] >= amount,
-                'Token Balance too low'
+                'token balance too low'
             );
         } else {
             require(
                 traderBalances[msg.sender][DAI] >= amount.mul(price),
-                'Dai Balance too low'
+                'dai balance too low'
             );
         }
         Order[] storage orders = orderBook[ticker][uint(side)];
@@ -102,26 +139,32 @@ contract Dex {
             amount,
             0,
             price,
-            block.timestamp
+            now
         ));
 
         uint i = orders.length > 0 ? orders.length - 1 : 0;
-        while (i > 0) {
-            if(side == Side.BUY && orders[i -1].price > orders[i].price) {
+        while(i > 0) {
+            if(side == Side.BUY && orders[i - 1].price > orders[i].price) {
                 break;
             }
-            if(side == Side.SELL && orders[i -1].price < orders[i].price) {
+            if(side == Side.SELL && orders[i - 1].price < orders[i].price) {
                 break;
             }
             Order memory order = orders[i - 1];
             orders[i - 1] = orders[i];
             orders[i] = order;
-            i = i.sub(1);
+            i--;
         }
-        nextOrderId = nextOrderId.add(1);
+        nextOrderId++;
     }
 
-    function createMarketOrder(bytes32 ticker, uint amount, Side side) tokenExist(ticker) tokenIsNotDai(ticker) external {
+    function createMarketOrder(
+        bytes32 ticker,
+        uint amount,
+        Side side)
+        tokenExist(ticker)
+        tokenIsNotDai(ticker)
+        external {
         if(side == Side.SELL) {
             require(
                 traderBalances[msg.sender][ticker] >= amount,
@@ -145,7 +188,7 @@ contract Dex {
                 msg.sender,
                 matched,
                 orders[i].price,
-                block.timestamp
+                now
             );
             if(side == Side.SELL) {
                 traderBalances[msg.sender][ticker] = traderBalances[msg.sender][ticker].sub(matched);
@@ -163,49 +206,29 @@ contract Dex {
                 traderBalances[orders[i].trader][ticker] = traderBalances[orders[i].trader][ticker].sub(matched);
                 traderBalances[orders[i].trader][DAI] = traderBalances[orders[i].trader][DAI].add(matched.mul(orders[i].price));
             }
-            nextTradeId = nextTradeId.add(1);
-            i = i.add(1);
+            nextTradeId++;
+            i++;
         }
 
         i = 0;
         while(i < orders.length && orders[i].filled == orders[i].amount) {
-            for(uint j = i; j < orders.length -1; j++) {
-                orders[j] = orders[j.add(1)];
+            for(uint j = i; j < orders.length - 1; j++ ) {
+                orders[j] = orders[j + 1];
             }
             orders.pop();
-            i = i.add(1);
+            i++;
         }
     }
 
-    function getOrders(bytes32 ticker, Side side) external view returns(Order[] memory) {
-        return orderBook[ticker][uint(side)];
-    }
-
-    function getTokens()
-      external
-      view
-      returns(Token[] memory) {
-      Token[] memory _tokens = new Token[](tokenList.length);
-      for (uint i = 0; i < tokenList.length; i++) {
-        _tokens[i] = Token(
-          tokens[tokenList[i]].ticker,
-          tokens[tokenList[i]].tokenAddress
-        );
-      }
-      return _tokens;
-    }
-
     modifier tokenIsNotDai(bytes32 ticker) {
-        require(
-            ticker != DAI, 'cannot trade DAI'
-        );
-        _;
+       require(ticker != DAI, 'cannot trade DAI');
+       _;
     }
 
     modifier tokenExist(bytes32 ticker) {
         require(
-             tokens[ticker].tokenAddress != address(0),
-             'this token does not exist'
+            tokens[ticker].tokenAddress != address(0),
+            'this token does not exist'
         );
         _;
     }
