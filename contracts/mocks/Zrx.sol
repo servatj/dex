784e707a4943b1e98272922ae16e0a4e2d6e0a06
Josep Servat <jservat@outlook.es>
2021-09-29 08:50:10 +0200

    Initial smart contract commit

---
+++ contracts/mocks/Zrx.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
+
+contract Zrx is ERC20 {
+    constructor() ERC20('Rep', 'Augur Token') public {
+        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+    }
+
+    function faucet(address to, uint amount) external {
+        _mint(to, amount);
+    }
+}
